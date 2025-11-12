165913af45f63c564b95c52174031585f9b90e1e
Abo Ellil <mr.abuellil@gmail.com>
2025-11-12 03:33:17 -0800

    build(customer): update babel config and add react-native-svg dependency
    Updated babel configuration to include jsxRuntime: 'automatic' option
    for Expo preset and added react-native-svg as a project dependency

--- customer/babel.config.js
+++ customer/babel.config.js
@@ -1,10 +1,17 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          jsxRuntime: 'automatic',
+        },
+      ],
+    ],
     plugins: [
       'nativewind/babel',
-      'react-native-reanimated/plugin', // Must be last
+      'react-native-reanimated/plugin',
     ],
   };
 };
